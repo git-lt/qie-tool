c28c1dfd070c8c8785817737a059be2735a00349
柳涛 <935486956@qq.com>
2018-09-15 11:12:57 +0800

    aa

--- packages/commands/publish.js
+++ packages/commands/publish.js
@@ -5,9 +5,10 @@ const axios = require('axios');
 const exists = require('fs').existsSync;
 const logger = require('../lib/logger');
 const inquirer = require('inquirer');
+const Spinner = require('../lib/spinner');
 
 const resolve = function (relativePath) { 
-    return path.resolve(process.cwd(), relativePath) 
+    return path.resolve(process.cwd(), relativePath);
 }
 
 module.exports = (env) => {
@@ -18,9 +19,14 @@ module.exports = (env) => {
 
     const config = require(resolve('qie.config.js'));
 
-    // config.publish.url
+    // 检查 url
+    if(!config.publish.url){
+        return logger.warn('请在配置文件 qie.config.js 中配置 publish.url')
+    }
+
     // config.publish.key
 
+    // 检查 index.html
     const indexHtmPath = resolve(`${config.upload.config.dir}/index.html`);
     if(!exists(indexHtmPath)){
         return logger.fatal(`读取 index.html 失败, 请查路径 ${indexHtmPath} 是否存在`)
@@ -45,13 +51,16 @@ module.exports = (env) => {
             envName: env,
             desc,
         };
-
+        const apiUrl =  path.join(config.publish.url, '/pub/version');
         axios.defaults.timeout = 30 * 1000;
         axios.defaults.responseType = 'json';
+
+        const spinner = new Spinner('正在发布...');
         axios
-            .post(server, params)
+            .post(apiUrl, params)
             .then(res => res.data)
             .then(res => {
+                spinner.stop();
                 if (res.success) {
                     delete res.data.content;
                     logger.success(`${JSON.stringify(res.data)}`);
@@ -60,6 +69,7 @@ module.exports = (env) => {
                 }
             })
             .catch(err => {
+                spinner.stop();
                 logger.fatal(err);
             });
     }).catch(err => {
